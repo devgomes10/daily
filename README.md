97cfbc8a3791c05c94210b656a9bdc61b19ac31b
Vinicius Gomes Correia <viniciusgomesccc10@gmail.com>
2024-02-08 17:13:39 -0300

    first project

---
+++ README.md
@@ -0,0 +1,22 @@
+A project daily using http
+
+## ✔️ techs
+- 
+- `API`
+- `http`
+- `async/await`
+- `interceptors`
+- `logger`
+
+## 🔨 Projeto: Simple Journal
+
+The Flutter code: Web API - Integrating your Application are **Classes** and **Functions** to generate an application of a **Personal Diary**, which **records daily entries** from the person using the application.
+
+## 🛠️ open the project
+
+**For starting the project, you need:**
+
+- Have an IDE, which can be [Android Studio](https://developer.android.com/) installed on your machine;
+- Have the [Flutter SDK](https://docs.flutter.dev/get-started/install) in version 3.0.0;
+- Have [Node.JS](https://nodejs.org/en/) installed on your machine;
+- Have a server [JSON-Server](https://www.npmjs.com/package/json-server) running the file [server/db.json] at an address visible to the emulator used;
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:daily/services/journal_service.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+
+  JournalService service = JournalService();
+  service.register("hello, world");
+  service.get();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
